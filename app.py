09652173a41e063d30f61092bbe4693021632a58
Caleb Ho <pcalebho@gmail.com>
2023-05-26 19:06:51 -0500

    Added dockerfile for sql database w/ sql script

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+import MySQLdb
 
 app = Flask(__name__)
 boostrap = Bootstrap(app)
+db = SQLAlchemy()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sql'
+    'mysql://root:sql22@6cfe002e4cea/materials_db'
 # Database can add new data after request
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  
-db = SQLAlchemy(app)
 
-class Materials(db.Model):
-    pass
+db.init_app(app)
 
-class Vendors(db.Model):
-    pass
+# class Materials(db.Model):
+#     pass
 
-class HeatTreatment(db.Model):
-    pass
+# class Vendors(db.Model):
+#     pass
+
+# class HeatTreatment(db.Model):
+#     pass
 
 #view function for base page
 @app.route('/')
 def root():
     return render_template('bootstrap_test.html', name = 'Caleb')
+
