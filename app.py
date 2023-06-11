6ef00f09a5104a22ed3faaab16359cc1bf5351c6
Caleb Ho <pcalebho@gmail.com>
2023-06-11 11:01:38 -0500

    added bootstrap and nosql database for testing

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-import MySQLdb
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 boostrap = Bootstrap(app)
 db = SQLAlchemy()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'mysql://root:sql22@6cfe002e4cea/materials_db'
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 # Database can add new data after request
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  
 
 db.init_app(app)
 
-# class Materials(db.Model):
-#     pass
+#This model is used to create a table and add rows
+class Materials(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    material_name = db.Column(db.String, unique=True, nullable=False)
+    ultimate_strength = db.Column(db.Integer)
+    yield_strength = db.Column(db.Integer)
+    
+
+    #Method used for outputting String representation 
+    def __repr__(self):
+        return '<Material Name: %r>' % self.material_name
 
 # class Vendors(db.Model):
 #     pass
@@ -26,5 +37,9 @@ db.init_app(app)
 #view function for base page
 @app.route('/')
 def root():
-    return render_template('bootstrap_test.html', name = 'Caleb')
+    return render_template('base_view.html')
 
+#view function for contact slide
+@app.route('/contact')
+def contact():
+    return '<h2> Contact me at pcalebho@gmail.com </h2>'
