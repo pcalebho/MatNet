4aa4554d434f5e823ce32d53b6c19fd854550a89
pcalebho <pcalebho@gmail.com>
2023-06-25 16:30:56 -0500

    added class id's

--- webscraper/MatWeb_scraper_script.py
+++ webscraper/MatWeb_scraper_script.py
@@ -8,4 +8,7 @@ url = 'https://matweb.com/search/QuickText.aspx?SearchText=' + searches[0]
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
+
+next_page_id='ctl00_ContentMain_ucSearchResults1_lnkNextPage'
+material_pages_link_id = 'lnkMatl'
 print(soup.prettify())
