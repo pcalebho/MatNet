6fe113f835e77844a30e9861edc568ce584a400c
pcalebho <pcalebho@gmail.com>
2023-06-25 23:28:09 -0500

    transformed into function for searching pages

--- webscraper/MatWeb_scraper_script.py
+++ webscraper/MatWeb_scraper_script.py
@@ -7,47 +7,57 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import click
 
-material_pages = []
+def search_material_pages(searches: list[str]) -> list[str]:
+    material_pages = []
+    url_list = []
 
-options = Options()
-options.add_experimental_option("detach",True)
-options.add_argument('--headless')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
+    options = Options()
+    options.add_experimental_option("detach",True)
+    options.add_argument('--headless')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
-searches = ['AISI', 'aluminum']
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 
-url = 'https://matweb.com/search/QuickText.aspx?SearchText=' + searches[0]
+    [url_list.append('https://matweb.com/search/QuickText.aspx?SearchText=' + search) for search in searches]
 
-driver.get(url)
+    i = 0
 
-#Number of pages are used for the end condition of the loop
-page_select_button = driver.find_element(By.NAME, 'ctl00$ContentMain$ucSearchResults1$drpPageSelect1')
-pages = page_select_button.find_elements(By.TAG_NAME, 'option')
+    for url in url_list:
+        driver.get(url)
 
-
-next_button = driver.find_element(By.ID, 'ctl00_ContentMain_ucSearchResults1_lnkNextPage')
-
-for page in pages:
-    next_button = driver.find_element(By.ID, 'ctl00_ContentMain_ucSearchResults1_lnkNextPage')
-    
-    next_button.click()
-    time.sleep(1)
-
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    #Find and store all the material links on the page 
-    for link in soup.find_all('a', id = re.compile('^lnkMatl')):
-        material_pages.append(link.get('href')) 
+        #Number of pages are used for the end condition of the loop
+        page_select_button = driver.find_element(By.NAME, 'ctl00$ContentMain$ucSearchResults1$drpPageSelect1')
+        pages = page_select_button.find_elements(By.TAG_NAME, 'option')
 
 
-print(material_pages)
-print('Number of Results: ', len(material_pages))
-driver.quit()
+        next_button = driver.find_element(By.ID, 'ctl00_ContentMain_ucSearchResults1_lnkNextPage')
+
+        bar_label = searches[i] + ' (%i/%i)' % (i+1  ,len(searches))
+        with click.progressbar(pages, label= bar_label) as bar:
+            for page in bar:
+                next_button = driver.find_element(By.ID, 'ctl00_ContentMain_ucSearchResults1_lnkNextPage')
+                
+                next_button.click()
+                time.sleep(1)
+
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+                #Find and store all the material links on the page 
+                for link in soup.find_all('a', id = re.compile('^lnkMatl')):
+                    material_pages.append(link.get('href')) 
+         
+        i += 1
+
+    driver.quit()
+    return(material_pages)
 
 
+if __name__ == '__main__':
+    result = search_material_pages(['AISI','aluminum'])
+    print('Number of results: ', len(result))
 
 # print(next_page_link)
 # print(soup.prettify())
