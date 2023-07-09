b1909d915afa9e3acf462f5b08e5e1820a37e14e
pcalebho <pcalebho@gmail.com>
2023-07-09 09:26:18 -0500

    added mongodb connection

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
 from pymongo.mongo_client import MongoClient
 from flask import Flask, render_template
-# from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 
+#Connecting and creating MongoDB client instance
 MONGODB_URI = "mongodb+srv://pcalebho:UISBvUYTesMft5AX@matcluster.5ygnbeg.mongodb.net/?retryWrites=true&w=majority"
-
 client = MongoClient(MONGODB_URI)
+
+
+material_db = client.material
+datasheets_collection = material_db.test_collection
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -15,13 +17,20 @@ app = Flask(__name__)
 
 num_sliders = 5
 material_properties = ["", "Elastic Modulus",
-                       "Yield Strength", "Weight", "Cost", "Ultimate Strength", "six"]
+                       "Yield Strength", "Desnity", "Cost", "Ultimate Strength", "Machineability"]
 
 
 @app.route('/', methods = ('GET','POST'))
 def root():
-    return render_template('index.html', material_properties=material_properties,
-                           matprop_len=len(material_properties), num_sliders=num_sliders)
+    datasheets = datasheets_collection.find()
+
+    return render_template(
+        'index.html', 
+        material_properties=material_properties,
+        matprop_len=len(material_properties), 
+        num_sliders=num_sliders,
+        datasheets=datasheets
+    )
 
 # view function for contact slide
 
