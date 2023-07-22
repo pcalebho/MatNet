287528ce9b73e9da4c7a9bd27a63bf1b9e26f414
pcalebho <pcalebho@gmail.com>
2023-07-22 15:53:08 -0500

    add table to site

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+import re
 from pymongo.mongo_client import MongoClient
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #Connecting and creating MongoDB client instance
 MONGODB_URI = "mongodb+srv://pcalebho:UISBvUYTesMft5AX@matcluster.5ygnbeg.mongodb.net/?retryWrites=true&w=majority"
@@ -22,19 +23,14 @@ material_properties = ["", "Elastic Modulus",
 
 @app.route('/', methods = ('GET','POST'))
 def root():
-    datasheets = datasheets_collection.find()
-
     return render_template(
         'index.html', 
         material_properties=material_properties,
         matprop_len=len(material_properties), 
-        num_sliders=num_sliders,
-        datasheets=datasheets
+        num_sliders=num_sliders
     )
 
 # view function for contact slide
-
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
@@ -43,3 +39,59 @@ def contact():
 @app.route('/documentation')
 def glossary():
     return render_template('glossary.html')
+
+
+@app.route('/api/data')
+def data():
+    query = {}
+
+    # search filter
+    search = request.args.get('search')
+    if search:
+        query = {'name': {'$regex': re.escape(search), '$options': 'i'}}
+
+    # sorting
+    sort = request.args.get('sort')
+    sort_query = {}
+    if sort:
+        for s in sort.split(','):
+            sort_direction = -1 if s[0] == '-' else 1
+            sort_name = s[1:]
+            if sort_name not in ['elastic_mod','yield_strength','ult_strength','cost','machineability']:
+                sort_name = 'elastic_mod'
+            sort_query[sort_name] = sort_direction
+
+    # pagination
+    start = request.args.get('start', type=int, default=0)
+    length = request.args.get('length', type=int, default=0)
+
+    # Perform the query with sorting, skip, and limit parameters
+    query_result = datasheets_collection.find(query)
+
+    # Check if start and length are not None and use the default values if necessary
+    if start is None:
+        start = 0
+    if length is None:
+        length = 0
+
+    # Sorting the query_result based on the 'sort_query' dictionary
+    if sort_query:
+        query_result = query_result.sort(list(sort_query.items()))
+
+    # Applying skip and limit after sorting
+    if start >= 0 and length >= 0:
+        query_result = query_result.skip(start).limit(length)
+
+    materials = []
+    for material in query_result:
+        material.pop('_id')
+        material.pop('link')
+        materials.append(material)
+
+    total = datasheets_collection.count_documents(query)
+
+    # response
+    return {
+        'data': materials,
+        'total': total,
+    }
