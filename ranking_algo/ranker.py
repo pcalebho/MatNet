b7de210a5f03b5fe2fa0e6418cfdef285a0e1274
pcalebho <pcalebho@gmail.com>
2023-07-23 21:04:34 -0500

    ranker function added and adapted from ipynb

--- ranking_algo/ranker.py
+++ ranking_algo/ranker.py
@@ -1,43 +1,76 @@
 import pandas as pd
 import skcriteria as skc
 import numpy as np
-import matplotlib.pyplot as plt
-import json
-import yaml
-
 from skcriteria.preprocessing import invert_objectives, scalers
 from skcriteria.madm import simple
 
-with open('webscraper/results_files/AISI_steels_fakedata.yaml','r') as stream:
-    try:
-        raw_data = yaml.safe_load(stream)
-    except FileNotFoundError:
-        raise FileNotFoundError
+def get_id(key):
+    criterion_key = {
+        'Name' : 'name',
+        'Elastic Modulus' : 'elastic_mod',
+        'Yield Strength' : 'yield_strength',
+        'Cost' : 'cost',
+        'Ultimate Strength': 'ult_strength',
+        'Machineability' : 'machineability'
+    }
 
-dataframe = pd.DataFrame(raw_data)
+    if type(key) == list:
+        result = [criterion_key.get(k) for k in key]
+        result = [x for x in result if x != None]
+        return result
 
-# let's says the first two alternatives are
-# for maximization and the last one for minimization
-objectives = [max, max, min]
+    return criterion_key.get(key)
 
-# we use the built-in function as aliases
-dm = skc.mkdm(
-    dataframe,
-    objectives,
-    weights=[0.5, 0.05, 0.45],
-)
-dm = dm.copy(alternatives=["VW", "Ford"])
+def rank_materials(criterions, weights, raw_data):
+    raw_dataframe = pd.DataFrame(raw_data)
+
+    #Filter out inconsequential data
+    formData = {}
+    for i in range(5):
+        if criterions[i] != '' and weights[i] != 0:
+            formData[criterions[i]] = weights[i]  
+
+    #Reformat and normalize weights
+    np_weights = np.asarray(list(formData.values()))
+    np_weights = np.divide(weights,10)
+
+    #get objectives from weights
+    objectives = []
+    for weight in np_weights:
+        if weight < 0:
+            objectives.append(min)
+        elif weight > 0:
+            objectives.append(max)
 
 
-#SKC handles maximization or minimization only best , so we invert the third row to be a maximization problem 
-#maximization is preferable to minimizationm
-inverter = invert_objectives.InvertMinimize()
-dmt = inverter.transform(dm)
-print(dmt)
+    criterion_ids = get_id(list(formData.keys()))
 
-#normalizes values
-scaler = scalers.SumScaler(target="both")
-dmt = scaler.transform(dmt)
+    df = raw_dataframe.loc[:, criterion_ids]        #type: ignore
 
-dec = simple.WeightedSumModel()
-rank = dec.evaluate(dmt)  # we use the tansformed version of the data
+    # we use the built-in function as aliases
+    row_id =  list(raw_dataframe.loc[:, 'name'])
+    dm = skc.mkdm(
+        df.to_numpy(),
+        objectives= objectives,
+        weights= weights,
+        alternatives = row_id,
+        criteria= list(formData.keys())
+    )
+
+    #SKC handles maximization or minimization only best , so we invert the third row to be a maximization problem 
+    #maximization is preferable to minimizationm
+    inverter = invert_objectives.InvertMinimize()
+    dmt = inverter.transform(dm)
+
+    #normalizes values
+    scaler = scalers.SumScaler(target="both")
+    dmt = scaler.transform(dmt)
+
+    #Get rank
+    dec = simple.WeightedSumModel()
+    rank = dec.evaluate(dmt)  # we use the tansformed version of the data
+
+    df_with_score = raw_dataframe.assign(Score = rank.e_.score).assign(Rank = rank.rank_)
+    SortedDF = df_with_score.sort_values(by = 'Score', ascending = False)
+
+    return SortedDF
