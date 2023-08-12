5322d4101640f71e6d41b8dc9a551e98aa4cfe3c
pcalebho <pcalebho@gmail.com>
2023-08-12 18:45:23 -0500

    add new db py

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.Text(), nullable=False)
+    industry = db.Column(db.String(80), nullable=False)
+    occupation = db.Column(db.String(20), nullable=False)
