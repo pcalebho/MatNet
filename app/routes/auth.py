6ef928d6cc6f13fedb1fd1581e938af30fac764d
pcalebho <pcalebho@gmail.com>
2023-08-13 15:42:03 -0500

    added register feature in login page

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -80,6 +80,7 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
+        login_user(new_user, remember=False)
         return redirect('/')
 
     return render_template('register.html', title='Register', form=form)
