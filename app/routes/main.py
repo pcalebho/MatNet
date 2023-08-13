9dd736eaf29089dc3e57f5d74d1ca4f2804ed4db
pcalebho <pcalebho@gmail.com>
2023-08-13 14:58:02 -0500

    login and logout page added to main page

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,5 +1,6 @@
 from flask import session, Blueprint, render_template
 from app.ranker import KEY
+from flask_login import current_user
 
 main_bp = Blueprint('main', __name__)
 
@@ -8,10 +9,11 @@ num_sliders = len(material_properties)
 
 @main_bp.route('/')
 def root(): 
-    session.clear()     
+    # session.clear()     
     return render_template(
         'index.html', 
         material_properties=material_properties,
         matprop_len=len(material_properties), 
-        num_sliders=num_sliders
+        num_sliders=num_sliders,
+        current_user=current_user
     )
