da0b836bb4ca076ca8be587ef4fb980703efa2fe
pcalebho <pcalebho@gmail.com>
2023-08-13 14:47:09 -0500

    added config option to fix db OperationalError

--- config.py
+++ config.py
@@ -14,8 +14,10 @@ class Config():
     MATERIAL_COLLECTION = os.environ.get('MATERIAL_COLLECTION')
     POSTGRESQL_URI= os.environ.get('POSTGRESQL_URI')
     
+    
     #Built-in Flask config values
     SQLALCHEMY_DATABASE_URI = POSTGRESQL_URI
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}  #Needed to stop the SQL disconnect issues
     SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = False
 
