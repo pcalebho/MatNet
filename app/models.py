d02bcfb46771c6ff63a5d37cb4a775d35c47d309
pcalebho <pcalebho@gmail.com>
2023-10-22 22:04:24 -0500

    add fatigue data model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from flask_login import UserMixin 
-from mongoengine import Document, StringField, EmailField
+from mongoengine import Document, StringField, EmailField, DictField, ListField
 class User(Document, UserMixin):
     email = EmailField(required=True, unique= True)
     password = StringField(required=True)
@@ -8,3 +8,16 @@ class User(Document, UserMixin):
 
     def get_id(self):
         return self.email
+    
+class Fatigue(Document):
+    description = StringField()
+    material_name = StringField()
+    product_form = StringField()
+    k_value = StringField()
+    tus = StringField(db_field = 'tus_ksi')
+    tys = StringField(db_field = 'tys_ksi')
+    temp = StringField(db_field='temp_F')
+    equations = DictField(db_field='equivalent_stress_equations')
+    graph = ListField()
+
+    meta = {'collection': 'fatigue_data'}
