e635d8d38dda29e66e3674d1131003818a09353e
pcalebho <pcalebho@gmail.com>
2023-11-08 09:13:23 -0600

    add graph popup function

--- app/static/popup.js
+++ app/static/popup.js
@@ -22,6 +22,30 @@ export let rowPopupFormatter = function(e, row, onRendered){
     return container;
 };
 
+export let graphPopupFormatter = function(e, row, onRendered){
+    let data = row.getData();
+    let categories = convertArrayToString(data.categories, " | ");
+    let compEleProp = data.component_elements_properties
+    const entries = Object.entries(compEleProp)
+    
+    
+    let container = document.createElement("div");
+    let contents = "<strong style='font-size:1.2em;'>Additional Details</strong><br/><ul style='padding:0;  margin-top:10px; margin-bottom:0;'>";
+    contents += "<li><strong>Categories:</strong> " + categories + "</li>";
+    contents += "<li><strong>Component Elements:</strong><ul>";
+    
+    for (const [element, percentage] of entries){
+        contents+= "<li>" + convertStr(element) + ": " + percentage + "</li>";
+    }
+    
+    contents += "</ul>";
+    contents += "</ul>";
+
+    container.innerHTML = contents;
+
+    return container;
+}
+
 function convertStr(str) {
     let eleName;
     let eleAbbr;
